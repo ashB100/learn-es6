aabb027d83f8e2ec194911ba4abad44256c0f0f3
Ashnita Bali <ashnita@Ashnitas-MBP.home>
2017-09-11 13:35:16 +0100

    complete convert.js

--- ConvertArrayLikeObjectToArray/convert.js
+++ ConvertArrayLikeObjectToArray/convert.js
@@ -7,5 +7,9 @@ const products = Array.from(document.querySelectorAll('.product'));
 // array methods that we want to use, like filter(), forEach, reduce
 // What we can do is convert this NodeList to Array using Array.from()
 // method provided in ES6.
+
 console.log(product); // [li.product, li.product, li.product]
 
+products
+    .filter(product => parseFloat(product.innerHTML) < 10)
+    .forEach(product => product.style.color = 'red');
